105dd2993c0d517a45053ea92871a1cc976f6782
morpheus <mariageorgepapas@gmail.com>
2020-09-08 22:57:03 -0500

    Switch statement (using int, char, and String as values)

---
+++ src/com/mariageorgepapas/Main.java
@@ -0,0 +1,75 @@
+package com.mariageorgepapas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int value = 1;
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Value was not 1 or 2");
+        }
+
+        // equivalent to the above
+
+        int switchValue = 1;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Value was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Value was not 1, 2, 3, 4, or 5");
+                break;
+        }
+
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+
+        char charValue = 'A';
+
+        switch (charValue){
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println(charValue + " was found");
+                break;
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break;
+        }
+
+        String month = "January";
+
+        switch (month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+                break;
+        }
+
+
+
+    }
+}
